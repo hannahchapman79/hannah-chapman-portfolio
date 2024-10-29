ac59dd61e05f967273bac96a9ba15b707b44d23f
hannahchapman79 <hannahchapman1999@aol.com>
2024-10-29 21:38:36 +0800

    Updates about me page with a component displaying Github contributions over last 6 months

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import TechStack from "./TechStack"
+import Github from "./Github"
+
+const AboutMe = () => {
+    return (
+        <>
+        <TechStack/>
+        <Github />
+        </>
+    )}
+
+
+    export default AboutMe
