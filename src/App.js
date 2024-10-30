77cf77ebd90e13456d4390e89c7d71acb1a022e6
hannahchapman79 <hannahchapman1999@aol.com>
2024-10-30 10:22:07 +0800

    Adds an unstyled  ProjectGrid component to hold seperate project cards that display individual project content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import NavBar from './components/Navbar';
 import { Route, Routes } from 'react-router-dom'
 import About from './components/about/About';
+import ProjectsGrid from "./components/projects/ProjectsGrid"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<ProjectsGrid />} />
       </Routes>
     </>
   )
