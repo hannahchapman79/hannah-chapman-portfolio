607b97098f5c75f15dbe153638772cb72551af8e
hannahchapman79 <hannahchapman1999@aol.com>
2024-11-05 11:15:31 +0800

    Adds styling to social icons and adds email icon

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import { FaLinkedin } from 'react-icons/fa';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+// import { FaLinkedin } from 'react-icons/fa';
+import { FiGithub } from "react-icons/fi";
+import { RiLinkedinBoxLine } from "react-icons/ri";
+import { MdMailOutline } from "react-icons/md";
+
 
 const SocialIcons = () => {
   return (
     <>
       <Container>
+        <p className="lets-connect">LET'S <span>CONNECT</span></p>
         <Row>
           <Col md={12}>
             <ul className="social-icons">
-              <li>
-                <a
-                  href="https://github.com/hannahchapman79"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <GitHubIcon className="home-icon"/>
-                </a>
-              </li>
               <li className="social-icons">
                 <a
                   href="https://www.linkedin.com/in/hannah-chapman-13360097/"
@@ -26,7 +22,26 @@ const SocialIcons = () => {
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
                 >
-                  <FaLinkedin className="home-icon"/>
+                  <RiLinkedinBoxLine className="home-icon linkedin-icon"/>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/hannahchapman79"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FiGithub className="home-icon"/>
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="mailto:hannahchapman1999@aol.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <MdMailOutline className="home-icon"/>
                 </a>
               </li>
             </ul>
