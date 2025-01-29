669c39f6a980f89e7954c1bca299c1dc53a6e1ac
hannahchapman79 <hannahchapman1999@aol.com>
2025-01-29 14:28:26 +0700

    Updates about me paragraph with AWS certification

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
       <section className="about-me-text" id="about-me">
         <h2 className="about-title">About Me, I'm <span>Hannah Chapman</span></h2>
         <p>I&apos;m from Leicester, UK and have over 4 years of <span>e-commerce</span> experience, which I&apos;ve balanced alongside a part-time <span>business management degree.</span> This combination has given me a well-rounded understanding of both business strategy and how technology can make a real difference.
-         </p> <p>Recently, I completed a <span>JavaScript</span> bootcamp at <span>Northcoders</span>, where I strengthened my development skills and learned to apply complex computer science concepts to my work.</p>
+         </p> <p>Recently, I completed a <span>JavaScript</span> bootcamp at <span>Northcoders</span>, where I strengthened my development skills and learned to apply complex computer science concepts to my work. I&apos;m also AWS Certified, expanding my knowledge of cloud technologies.</p>
         <p>When I&apos;m not coding, you&apos;ll find me practicing <span>yoga</span> and <span>pilates</span>, <span>traveling</span> to discover new places and cuisines, or <span>volunteering</span> for causes I care about. </p>
         <p>Thanks for visiting my portfolio—let&apos;s connect!</p>
       </section>
