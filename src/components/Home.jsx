831ee2f0599022f3cd3f80977ee1f595a2a7d339
hannahchapman79 <hannahchapman1999@aol.com>
2025-05-01 18:15:17 +0700

    Update Homepage headline

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
          <section className='intro-card'>
             <img className="hannah-icon" src="/assets/hannah-icon.png" alt="Hannah Chapman portrait" />
             <h1 className='name-intro'>Hi, I'm Hannah</h1>
-            <h1 className='role-intro'>Junior Fullstack Developer from the UK</h1>
-            <h2 className='personal-profile-intro'>I am a junior developer with a background in e-commerce, having worked across various industries where I saw firsthand the impact of well-crafted code on transforming business performance and user experience. This sparked my passion for bringing ideas to life through software development while using my love for continuous learning, critical thinking, and problem-solving.</h2>
+            <h1 className='role-intro'>Fullstack Developer from the UK</h1>
+            <h2 className='personal-profile-intro'>I am a developer with a background in e-commerce, having worked across various industries where I saw firsthand the impact of well-crafted code on transforming business performance and user experience. This sparked my passion for bringing ideas to life through software development while using my love for continuous learning, critical thinking, and problem-solving.</h2>
          </section>
       </div>
          <SocialIcons/>
