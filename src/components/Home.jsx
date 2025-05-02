d4cce4817a52112acaefab97dc37033180250301
hannahchapman79 <hannahchapman1999@aol.com>
2025-05-02 09:02:10 +0700

    Update Home text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <img className="hannah-icon" src="/assets/hannah-icon.png" alt="Hannah Chapman portrait" />
             <h1 className='name-intro'>Hi, I'm Hannah</h1>
             <h1 className='role-intro'>Fullstack Developer from the UK</h1>
-            <h2 className='personal-profile-intro'>I am a developer with a background in e-commerce, having worked across various industries where I saw firsthand the impact of well-crafted code on transforming business performance and user experience. This sparked my passion for bringing ideas to life through software development while using my love for continuous learning, critical thinking, and problem-solving.</h2>
+            <h2 className='personal-profile-intro'>I am a developer with a background in CRO and e-commerce, having worked across various industries where I saw firsthand the impact of well-crafted code on transforming business performance and user experience. This sparked my passion for bringing ideas to life through software development while using my love for continuous learning, critical thinking, and problem-solving.</h2>
          </section>
       </div>
          <SocialIcons/>
